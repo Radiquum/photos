165e68b9f42c967042cf6a530e2c3b21dc06b90c
Kentai Radiquum <kentai.waah@gmail.com>
2025-02-22 00:13:12 +0500

    feat/generator: change tag button style

--- generate/templates/Tags.tsx
+++ generate/templates/Tags.tsx
@@ -1,9 +1,9 @@
 export default function Tags({ tags }: { tags: string[] }) {
   return (
     <div className="container mx-auto">
-      <div className="flex overflow-x-auto px-8 py-2 gap-2 scrollbar-thin scrollbar-corner-rounded-sm scrollbar-thumb-gray-700 scrollbar-track-gray-900">
+      <div className="flex overflow-x-auto px-4 py-2 gap-2 scrollbar-thin scrollbar-corner-rounded-sm scrollbar-thumb-gray-700 scrollbar-track-gray-900">
         <button
-          className="bg-gray-800 rounded-full px-3 py-1 text-sm"
+          className="bg-gray-800 rounded-lg px-4 py-1 text-sm"
           key="all"
           data-type="tag"
           data-tag="all"
@@ -12,7 +12,7 @@ export default function Tags({ tags }: { tags: string[] }) {
         </button>
         {tags.map((tag) => (
           <button
-            className="bg-gray-800 rounded-full px-3 py-1 text-sm hidden"
+            className="bg-gray-800 rounded-lg px-4 py-1 text-sm hidden"
             key={tag}
             data-type="tag"
             data-tag={tag}
